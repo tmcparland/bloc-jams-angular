09912093d2080066c726ebeb425a8d65895fb2b6
Tara <tara.mcparland@gmail.com>
2017-11-13 22:34:57 -0500

    added collection state

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,27 +1,28 @@
-function config($stateProvider, $locationProvider) {
+(function() {
+  function config($stateProvider, $locationProvider) {
     $locationProvider
-        .html5Mode({
-            enabled: true,
-            requireBase: false
-        });
-}
+      .html5Mode({
+        enabled: true,
+        requireBase: false
+      });
 
-$stateProvider
-    .state('landing', {
+    $stateProvider
+      .state('landing', {
         url: '/',
         templateUrl: '/templates/landing.html'
       })
       .state('album', {
-          url: '/album',
-          templateUrl: '/templates/album.html'
+        url: '/album',
+        templateUrl: '/templates/album.html'
+      })
+      .state('collection', {
+        url: '/collection',
+        templateUrl: '/templates/collection.html'
       });
+  }
 
-(function() {
-    function config($stateProvider, $locationProvider) {
-    }
-
-angular.module('blocJams', ['ui.router']);
-    angular
-        .module('blocJams', ['ui.router'])
-        .config(config);
+  angular.module('blocJams', ['ui.router']);
+  angular
+    .module('blocJams', ['ui.router'])
+    .config(config);
 })();
